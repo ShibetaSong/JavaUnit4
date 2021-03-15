8a5c2bde3572cb15c2de476452cf789caecad910
Shibeta <shibeta_song@163.com>
2021-03-15 08:05:15 +0800

    修改了用户名和密码的属性； 移除了有关用户密码的增删方法； 新增了从房间中同步住户至用户名的方法；

--- src/com/shibeta/unit4/HotelManagement/module/User.java
+++ src/com/shibeta/unit4/HotelManagement/module/User.java
@@ -1,8 +1,20 @@
 package com.shibeta.unit4.HotelManagement.module;
 
+import com.shibeta.unit4.HotelManagement.view.InformationMessage;
+
+import java.util.Arrays;
+
 public class User {
-    public static final String[] userName = {"user1", "user2"};
-    public static final String[] userPasswd = {"passwd1", "passwd2"};
+    private static String[] userNames = {};
+    private static String[] userPasswords = {};
+
+    public static String[] getUserNames() {
+        return userNames;
+    }
+
+    public static String[] getUserPasswords() {
+        return userPasswords;
+    }
 
     /**
      * 查询房间信息
@@ -14,18 +26,39 @@ public class User {
      */
     public static void showService() {}
 
-    /**
-     * 设置用户名
-     */
-    public static void setUsername() {}
+    public static String[][] setUserInformation(RoomList room) {
+        if (room != null) {
+            StringBuilder userName = new StringBuilder();
+            StringBuilder userPasswd = new StringBuilder();
+            if (!(room.getUsername().equals(InformationMessage.unsettle()))) {
+                userName.append(room.getUsername());
+                userName.append(" ");
+                userPasswd.append(room.getUserIdentify());
+                userPasswd.append(" ");
+            }
 
-    /**
-     * 设置用户密码
-     */
-    public static void setUserPasswd() {}
-
-    /**
-     * 获取用户密码
-     */
-    public static void getUserPasswd() {}
+            if (room.nextRoom != null) {
+                String[][] userInformation = setUserInformation(room.nextRoom);
+                if (userInformation.length > 0) {
+                    if (!(Arrays.toString(userInformation[0]).equals("[\"\"]") || userInformation[0] == null)) {
+                        for (String name : userInformation[0]) {
+                            userName.append(name);
+                        }
+                        for (String passwd : userInformation[1]) {
+                            userPasswd.append(passwd);
+                        }
+                    }
+                    userNames = userName.toString().split(" ");
+                    userPasswords = userPasswd.toString().split(" ");
+                    return new String[][]{userNames, userPasswords};
+                }
+            }
+            if (!(userName.toString().equals(""))) {
+                userNames = userName.toString().split(" ");
+                userPasswords = userPasswd.toString().split(" ");
+                return new String[][]{userNames, userPasswords};
+            }
+        }
+        return new String[][]{};
+    }
 }
