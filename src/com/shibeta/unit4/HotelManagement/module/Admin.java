35b01166928e46445760d5c936120cad00f5bdd0
Shibeta <shibeta_song@163.com>
2021-03-22 11:11:49 +0800

    新增了用于存储数据的Data类； 完善了获取房间信息的方法；

--- src/com/shibeta/unit4/HotelManagement/module/Admin.java
+++ src/com/shibeta/unit4/HotelManagement/module/Admin.java
@@ -1,14 +1,18 @@
 package com.shibeta.unit4.HotelManagement.module;
 
+import com.shibeta.unit4.HotelManagement.view.InformationMessage;
 import com.shibeta.unit4.HotelManagement.view.Menu;
 
 public class Admin {
-    StringBuilder adminPassword = new StringBuilder("admin123");
+    static Data data = new Data();
+    private static String newAdminPasswd = data.getAdminPasswd();
+    StringBuilder adminPassword = new StringBuilder(newAdminPasswd);
 
     public Admin() {}
 
     public void setAdminPasswd(String password) {
-        this.adminPassword = new StringBuilder(password);
+        data.setAdminPasswd(password);
+        newAdminPasswd = data.getAdminPasswd();
     }
 
     public String getAdminPasswd() {
@@ -25,13 +29,25 @@ public class Admin {
                 room.getRoomFacilitiesRepair(roomIdentify));
     }
 
-    public void getRoomInformation(RoomList room) {
+    public void getRoomInformation(RoomList room, boolean allowEmpty) {
+        if (allowEmpty) {
+            if (room != null) {
+                getRoomInformation(room, room.getRoomIdentify());
+                if (room.nextRoom != null) {
+                    getRoomInformation(room.nextRoom, true);
+                }
+            }
+            return;
+        }
         if (room != null) {
-            getRoomInformation(room, room.getRoomIdentify());
+            if (!(room.getUsername().equals(InformationMessage.unsettle()))) {
+                getRoomInformation(room, room.getRoomIdentify());
+            }
             if (room.nextRoom != null) {
-                getRoomInformation(room.nextRoom);
+                getRoomInformation(room.nextRoom, false);
             }
         }
+
     }
 
 }
